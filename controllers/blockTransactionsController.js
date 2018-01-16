396a3b59ec9c2142a75781495f9789050c540089
Nick Halvorsen <lingosalad@gmail.com>
2018-01-16 18:25:55 -0500

    remove unneeded console.log

--- controllers/blockTransactionsController.js
+++ controllers/blockTransactionsController.js
@@ -2,7 +2,6 @@ var ethHelper = require('../services/ethHelper');
 
 exports.index = async function(req, res) {
     var transactionData = await ethHelper.getBlockTransactionData(req.params.block);
-    console.log(transactionData);
   	res.render('blockTransactions', { block: req.params.block, transactions: transactionData });
 };
 
